1504376278ac8471407f1c30db1f0f4323998160
Vladimir Chuguevec <777helk@mail.ru>
2023-02-01 20:47:16 +0300

    init

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,31 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
-
+@NamedQuery(name = "getAllCount", query = "SELECT count(p) from Player p")
+@Entity
+@Table(name = "player", schema = "rpg")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false, length = 12)
     private String name;
-
+    @Column(nullable = false, length = 30)
     private String title;
-
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Race race;
-
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Profession profession;
-
+    @Column(nullable = false)
     private Date birthday;
-
+    @Column(nullable = false)
     private Boolean banned;
-
+    @Column(nullable = false)
     private Integer level;
 
     public Player() {
